64e734c387f09b50ff78efd94d35ce0c23716731
arif akbarul huda <omayib@gmail.com>
2017-03-01 19:14:40 +0700

    Initial Commit

---
+++ MainFramework/MainFramework.h
@@ -0,0 +1,19 @@
+//
+//  MainFramework.h
+//  MainFramework
+//
+//  Created by qiscus on 3/1/17.
+//  Copyright © 2017 qiscus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MainFramework.
+FOUNDATION_EXPORT double MainFrameworkVersionNumber;
+
+//! Project version string for MainFramework.
+FOUNDATION_EXPORT const unsigned char MainFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MainFramework/PublicHeader.h>
+
+
